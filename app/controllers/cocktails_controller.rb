38c369f49a60ae92c55118f63eee4022b76ccb92
Tom Harwood <tharwood26@gmail.com>
2020-02-20 21:38:35 +0000

    style updates

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    # redirect_to cocktails_url, notice: 'Cocktail was destroyed.'
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :doses, :ingredients)
+  end
+end
